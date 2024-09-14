efea0dc00ba4e5cc7857395eeebce0acc4d29ed9
Fieldzzy <119826673+Fieldzzy@users.noreply.github.com>
2024-09-14 09:47:35 +0800

    Update fig3.py
    Algorithm calculating correlation function is changed from build-in algorithm to the propagator algorithm.

--- fig3.py
+++ fig3.py
@@ -2,6 +2,7 @@ import numpy as np
 import qutip as qt
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
+from functions import *
 
 omg0 = 1
 omgx = omg0
@@ -21,8 +22,9 @@ sigmax = qt.tensor(qt.qeye(dim), qt.sigmax())
 sigmaz = qt.tensor(qt.qeye(dim), qt.sigmaz())
 
 
-def main(omg):
-    omgd = 0.85 * omg0
+def main():
+    # |0> - |1> : 0.844, - |2> : 1.153, -|3> : 1.787
+    omgd = 1.15 * omg0
     H0 = omg0 * ad * a + omgx * sigmap * sigmam + g * (a + ad) * (np.cos(theta) * sigmaz - np.sin(theta) * sigmax)
     Hdrive = qt.QobjEvo([[Omg * a, lambda t: np.cos(omgd * t)], [Omg * ad, lambda t: np.cos(omgd * t)]])
     Hs = H0 + Hdrive
@@ -44,40 +46,12 @@ def main(omg):
                 La = La + Gamma_a * qt.lindblad_dissipator(eigenstates[j] * eigenstates[k].dag())
                 Ls = Ls + Gamma_s * qt.lindblad_dissipator(eigenstates[j] * eigenstates[k].dag())
 
-    t = np.linspace(0, 40, 200)
-    rho0 = qt.tensor(qt.fock_dm(dim, 0), qt.fock_dm(2, 0))
-
-    # L = qt.liouvillian(Hs)
-    # result = qt.mesolve(L, rho0, t, [La, Ls], ad * a)
-    # plt.plot(t, result.expect[0])
-    # plt.show()
 
     L00 = qt.liouvillian(H0)
     L0 = L00 + Ls + La
 
-    L_p = L_m = qt.liouvillian(0.5 * Omg * (a + ad))
-
-    #  Original method calculating steady state, may inaccurate
-    # Generate Sns and Tns
-    # Calculate steady state
-    # N = 10  # Maximum number of nonzero Sn
-    # S = T = list(np.zeros(N+2))
-    # S[N] = T[N] = qt.spre(qt.qzero([dim, 2]))
-    # temp = range(0, N)
-    # for i in reversed(temp):
-    #     Temp_SuperS = L0 - 1j * (i+1) * omgd * qt.spre(qt.qeye([dim, 2])) + L_m * S[i+2]
-    #     Temp_SuperT = L0 + 1j * (i+1) * omgd * qt.spre(qt.qeye([dim, 2])) + L_p * T[i+2]
-    #     S[i+1] = - Temp_SuperS.inv() * L_p
-    #     T[i+1] = - Temp_SuperT.inv() * L_m
-    # Temp = L0 + L_m * S[1] + L_p * T[1]
-    # rho_inf = Temp.inv() * qt.operator_to_vector(rho0)
-    # rho_inf_dm = qt.vector_to_operator(rho_inf)
-    # rho_ss = rho_inf_dm.unit()
-
     rho_ss = qt.steadystate_floquet(L00, [La, Ls], Omg * (a + ad), w_d=omgd)
     rho_ss = rho_ss.unit()
-
-    # print((rho_ss - rho_inf_dm).norm())
 
     # Calculate positive and negative components of X
     X = -1j * X0 * (a - ad)
@@ -91,22 +65,28 @@ def main(omg):
                 X_dot_m = X_dot_p.dag()
 
     # g20 = (rho_ss * X_dot_m * X_dot_m * X_dot_p * X_dot_p).tr() / ((rho_ss * X_dot_m * X_dot_p).tr()) ** 2
-    tau_list = np.linspace(0, 1000, 1000)
-    corr_list = qt.correlation_2op_1t(H0, rho_ss, tau_list, [La, Ls], X_dot_m, X_dot_p, reverse=True)
-    ker_list = corr_list * np.exp(1j * omg * tau_list)
-    return np.abs(2 * integrate.simpson(ker_list, x=tau_list))
+
+    prop = propagator(Hs, tau_list, [La, Ls])
+    g2a = np.zeros(points, complex)
+    for i in range(0, points):
+        g2a[i] = (rho_ss * X_dot_m * qt.vector_to_operator(prop[i].dag() * qt.operator_to_vector(X_dot_m))\
+                 * qt.vector_to_operator(prop[i].dag() * qt.operator_to_vector(X_dot_p)) * X_dot_p).tr() \
+                / ((rho_ss * X_dot_m * X_dot_p).tr()) ** 2
+
+    g2b = qt.correlation_3op_1t(L00, rho_ss, tau_list, [La, Ls], X_dot_m, X_dot_m * X_dot_p, X_dot_p)
+    g2b = g2b / ((rho_ss * X_dot_m * X_dot_p).tr()) ** 2
+    return g2a, g2b
 
 
-points = 1
-omg_list = np.linspace(0.3 * omg0, 1.7 * omg0, points)
-results = list(np.zeros(points))
-for i in range(0, points):
-    results[i] = main(omg_list[i])
-    print(i)
+global tau_list
+points = 2000
+tau_range = 1000
+tau_list = np.linspace(0, tau_range, points)
+g2a, g2b = main()
 
-results = np.array(results)
-fig, ax = plt.subplots()
-ax.semilogy(omg_list, results)
+fig, ax = plt.subplots(2, 1)
+ax[0].semilogy(tau_list, np.abs(g2a))
+ax[1].semilogy(tau_list, np.abs(g2b))
 plt.show(block=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
